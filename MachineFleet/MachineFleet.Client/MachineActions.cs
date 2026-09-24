d4bab4f5f9ce6cd9d8bd35c731d31b15a0f58ddd
Mohamedaliabdikarim <143351707+Mohamedaliabdikarim@users.noreply.github.com>
2026-09-24 08:44:46 +0200

    Add machine controls with cascading actions

---
+++ MachineFleet/MachineFleet.Client/MachineActions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MachineFleet.Client;
+
+public class MachineActions
+{
+    public EventCallback<Guid> OnStart { get; init; }
+    public EventCallback<Guid> OnStop { get; init; }
+    public EventCallback<Guid> OnSendData { get; init; }
+    public EventCallback<Guid> OnDelete { get; init; }
+}
