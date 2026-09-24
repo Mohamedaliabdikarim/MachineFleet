4582b7f7c8659ebc4ad9bb4571937ab7d9cb40d2
Mohamedaliabdikarim <143351707+Mohamedaliabdikarim@users.noreply.github.com>
2026-09-24 08:26:36 +0200

    Remove sample pages and add Machine model

---
+++ MachineFleet/MachineFleet.Client/Models/Machine.cs
@@ -0,0 +1,10 @@
+﻿namespace MachineFleet.Client.Models;
+
+public class Machine
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public required string Name { get; set; }
+    public bool IsOnline { get; set; }
+    public string LastData { get; set; } = string.Empty;
+    public DateTime LastUpdated { get; set; } = DateTime.Now;
+}
