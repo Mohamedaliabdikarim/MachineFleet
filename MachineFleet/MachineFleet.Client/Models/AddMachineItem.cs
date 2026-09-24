a56328b7ca8fe8b7a9f384da2b6b71195aecb3bc
Mohamedaliabdikarim <143351707+Mohamedaliabdikarim@users.noreply.github.com>
2026-09-24 08:48:55 +0200

    Add machine form with validation

---
+++ MachineFleet/MachineFleet.Client/Models/AddMachineItem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MachineFleet.Client.Models;
+
+public class AddMachineItem
+{
+    [Required(ErrorMessage = "Please enter a machine name.")]
+    [MinLength(2, ErrorMessage = "Name must be at least 2 characters.")]
+    [MaxLength(30, ErrorMessage = "Name can't be longer than 30 characters.")]
+    public string Name { get; set; } = string.Empty;
+}
--- MachineFleet/MachineFleet.Client/Models/Machine.cs
+++ MachineFleet/MachineFleet.Client/Models/Machine.cs
@@ -5,6 +5,6 @@ public class Machine
     public Guid Id { get; init; } = Guid.NewGuid();
     public required string Name { get; set; }
     public bool IsOnline { get; set; }
-    public string LastData { get; set; } = string.Empty;
+    public string LastData { get; set; } = "No data yet";
     public DateTime LastUpdated { get; set; } = DateTime.Now;
 }
